b9109aeb1ade4bd7df1a787c5e31b6e4a63d9eef
Jeeva R <jeeva.r@acheron-tech.com>
2021-11-24 21:22:12 +0530

    (feat:resource allocation): [FTP1-55] Resource Allocation Modal is created

--- projects/resource-manager/src/lib/employee-allocation/employee-allocation.component.ts
+++ projects/resource-manager/src/lib/employee-allocation/employee-allocation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Availability } from '../models/enums/availability';
+import { Resource } from '../models/resources/resource';
 
 @Component({
   selector: 'lib-employee-allocation',
@@ -6,7 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-allocation.component.scss'],
 })
 export class EmployeeAllocationComponent implements OnInit {
-  constructor() {}
-
+  resourceAllocationForm: FormGroup;
+  constructor(public dialog: MatDialog, public formBuilder: FormBuilder) {
+    this.resourceAllocationForm = formBuilder.group({
+      resource: new FormControl(''),
+      allocation: new FormControl(''),
+      role: new FormControl(''),
+      status: new FormControl(),
+      priority: new FormControl(''),
+      plannedStartDate: new FormControl(''),
+      plannedEndDate: new FormControl(''),
+      actualStartDate: new FormControl(''),
+      actualEndDate: new FormControl(''),
+      description: new FormControl(''),
+    }); 
+  }
+  startDate = new Date(1990, 0, 1);
+  selectedValue: string = '';
+  resources: Resource[] = [
+    {
+      resourceId: 1,
+      resourceName: 'James',
+      resourceType: 'Chef',
+      availability: Availability.CUSTOM,
+    },
+    {
+      resourceId: 2,
+      resourceName: 'Andy',
+      resourceType: 'Driver',
+      availability: Availability.CUSTOM,
+    },
+    {
+      resourceId: 3,
+      resourceName: 'Sam',
+      resourceType: 'Chef',
+      availability: Availability.CUSTOM,
+    },
+  ];
+  statuses: string[] = ['DEFINED', 'IN_PROGRESS'];
+  priorities: string[] = ['LOW', 'MEDIUM', 'HIGH'];
   ngOnInit(): void {}
 }
--- projects/resource-manager/src/lib/header/header.component.ts
+++ projects/resource-manager/src/lib/header/header.component.ts
@@ -4,6 +4,7 @@ import { format } from 'date-fns';
 import { MatDialog } from '@angular/material/dialog';
 import { DisplayFieldsComponent } from '../display-fields/display-fields.component';
 import { TaskAllocationComponent } from '../task-allocation/task-allocation.component';
+import { EmployeeAllocationComponent } from '../employee-allocation/employee-allocation.component';
 
 @Component({
   selector: 'lib-header',
@@ -42,6 +43,20 @@ export class HeaderComponent implements OnInit {
     const dialogRef = this.dialog.open(TaskAllocationComponent, {
       height: '100vh',
       width: '40vw',
+      position: {
+        right: '0',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(EmployeeAllocationComponent, {
+      height: '100vh',
+      width: '40vw',
+      panelClass: 'custom-dialog-container',
       position: {
         right: '0',
       },
