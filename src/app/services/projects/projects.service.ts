9755e43ee048f68f34259f54c5c584383723d91a
Tharunya REDDY <tharunya.reddy@acheron-tech.com>
2021-11-30 14:14:26 +0530

    projects component updated

--- src/app/services/projects/projects.service.ts
+++ src/app/services/projects/projects.service.ts
@@ -1,9 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Campaign } from 'projects/resource-manager/src/lib/models/campaigns/campaign';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectsService {
-
-  constructor() { }
+  constructor() {}
 }
